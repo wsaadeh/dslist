0922617ac14238b1dc7584d3be3c4926f91fb54e
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-01-22 17:18:50 -0300

    Get game by id

--- src/main/java/com/saadeh/dslist/services/GameService.java
+++ src/main/java/com/saadeh/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.saadeh.dslist.dto.GameDTO;
 import com.saadeh.dslist.dto.GameMinDTO;
 import com.saadeh.dslist.entities.Game;
 import com.saadeh.dslist.repositories.GameRepository;
@@ -16,6 +18,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();		
