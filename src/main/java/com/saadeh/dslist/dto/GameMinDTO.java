5d55c5e029522c57835823f62ce89c8d70d414c3
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-01-23 11:05:37 -0300

    Deploy ready

--- src/main/java/com/saadeh/dslist/dto/GameMinDTO.java
+++ src/main/java/com/saadeh/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.saadeh.dslist.dto;
 
 import com.saadeh.dslist.entities.Game;
+import com.saadeh.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -22,6 +23,14 @@ public class GameMinDTO {
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
 		longDescription = entity.getLongDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl =  projection.getImgUrl();
+		shortDescription = projection.getShortDescription();		
 	}
 
 	public Long getId() {
